ee1d0d6e6c8be20f204b731bca92834b78163d55
Tejas V <tejas.rv@outlook.com>
2022-04-03 17:48:43 +0530

    Updated the repo structure as per template

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+void PrintChar(char ch);
+
+void CGPA();
+void TrigCalc();
+void SciCalc();
+void EnggLabCalc();
+
+#endif
